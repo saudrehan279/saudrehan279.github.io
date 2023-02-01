fc2b3d6710a54260ba71ae1545d80b410061b598
Haider <saoodrehan@gmail.com>
2023-02-01 12:39:44 +0500

    commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser")
 
 
 const App = express();
+App.use(bodyParser.urlencoded({ extended: false }));
+App.use(bodyParser.json());
 App.use(cors({
      origin: true
 }));
@@ -16,6 +19,14 @@ App.get("/get", async (req, res) => {
 
 App.get("/", async (req, res) => {
      res.status(200).send("Hello There!")
+})
+
+App.get("/post", async (req, res) => {
+     let body = await req.body;
+
+     res.status(200).send({
+          ...body
+     })
 })
 
 
